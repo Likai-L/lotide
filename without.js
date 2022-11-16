e3ed41c4726800de94f025a14296bc8103feb489
Likai Liu <likailiu2@gmail.com>
2022-11-16 06:09:30 +0000

    Implement a function that filters a source array and returns a new array without the elements that are in the itemsToRemove array

---
+++ without.js
@@ -0,0 +1,39 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i in arr1) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`💃💃💃Yay!! They're equal!💃💃💃`);
+    return;
+  }
+  console.log(`🙅🙅🙅Nah!! They aren't euqal!🙅🙅🙅`);
+};
+
+const without = function(sourceArr, itemsToRemove) {
+  let newArr = [];
+  for (let value of sourceArr) {
+    if (!itemsToRemove.includes(value)) {
+      newArr.push(value);
+    }
+  }
+  return newArr;
+};
+
+// test codes, should all pass
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+assertArraysEqual(without([1, 2, 3], []), [1, 2, 3]);
+assertArraysEqual(without([], [1 ,2, 3]), []);
+assertArraysEqual(without([19, undefined, null, "NFR!", "Blue Banister"], ["Blue Banister", null, "19"]), [19, undefined, "NFR!"]);
+const words = ["hello", "world", "lighthouse"];
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
