2011cc08ca7ef5e095c9337b94dc3bbe7c3a6641
Likai Liu <likailiu2@gmail.com>
2022-11-16 05:05:36 +0000

    Implement a function that compares two arrays for a perfect(===) match

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🤑🤑Assertion Passed🤑🤑: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`💀💀Assertion Failed💀💀: [${actual}] !== [${expected}]`);
+  }
+};
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i in arr1) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// test cases, should all pass
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
