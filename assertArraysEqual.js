42d443f7c834630d71a07b7ad35182385ab37d78
Likai Liu <likailiu2@gmail.com>
2022-11-16 05:29:56 +0000

    Implement a function that compares two arrays and prints appropriate messages accordingly

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i in arr1) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`💃💃💃Yay!! They're equal!💃💃💃`);
+    return;
+  }
+  console.log(`🙅🙅🙅Nah!! They aren't euqal!🙅🙅🙅`);
+};
+
+// test codes
+assertArraysEqual([], []);
+assertArraysEqual([1], [1]);
+assertArraysEqual([1, 2], [1, "2"]);
+assertArraysEqual(["Taylor Swift", "Lana Del Rey"], ["Taylor Swift", "Lana Del Rey"]);
+assertArraysEqual(["Taylor Swift"], ["Lana Del Rey"]);
