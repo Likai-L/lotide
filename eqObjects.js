c6fb3f53640bd409a3e349c0ece67e81f0539c30
Likai Liu <likailiu2@gmail.com>
2022-11-20 11:27:43 +0000

    Modify eqObjects so it works when there are nested objects inside

--- eqObjects.js
+++ eqObjects.js
@@ -19,21 +19,29 @@ const eqArrays = function(arr1, arr2) {
 };
 
 const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length === Object.keys(object2).length) {
-    for (let key of Object.keys(object1)) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      } else {
-        if (object1[key] !== object2[key]) {
-          return false;
-        }
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (let key in object1) {
+    if (typeof object1[key] !== typeof object2[key]) {
+      return false;
+    }
+    if (Array.isArray(object1[key]) !== Array.isArray(object2[key])) {
+      return false;
+    }
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
       }
     }
-    return true;
+    if (typeof object1[key] === "object") {
+      return eqObjects(object1[key], object2[key]);
+    }
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
   }
-  return false;
+  return true;
 };
 
 // test code: primitie values
@@ -50,3 +58,11 @@ assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false);
+
+// test code: nested object values
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
+const nestedObj1 = {layer: 1, subObj: {layer: "purple", subObj: {layer: "blue", subObj: {layer: "yellow"}}}};
+const nestedObj2 = {subObj: {layer: "purple", subObj: {subObj: {layer: "yellow"}, layer: "blue"}}, layer: 1};
+assertEqual(eqObjects(nestedObj1, nestedObj2), true);
