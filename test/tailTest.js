fb988fe777dd7eccf3d0d0dd3c79fb71e5870fdf
Likai Liu <likailiu2@gmail.com>
2022-11-22 03:12:17 +0000

    Refactor the tests to use Mocha & Chai

---
+++ test/tailTest.js
@@ -0,0 +1,15 @@
+const tail = require("../tail");
+const assert = require("chai").assert;
+describe("#tail", () => {
+  it('returns ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it('returns [] for ["Taylor Swift"]', () => {
+    assert.deepEqual(tail(["Taylor Swift"]), []);
+  });
+
+  it('returns [] for []', () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
