5ee208e107775ab83ccf74e3a0e7e23cac73ade1
Likai Liu <likailiu2@gmail.com>
2022-11-22 00:21:28 +0000

    Modularize assertEqual and head: instead of copying and pasting the function around, now exports and require are used to access functions from different modules; seperate function code and test code

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Taylor Swift is my mother", "Taylor Swift is my mother");
-assertEqual("Taylor Swift is my mother", "Lana Del Rey is my mother");
-assertEqual(19, 22);
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require("../assertEqual");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Taylor Swift is my mother", "Taylor Swift is my mother");
+assertEqual("Taylor Swift is my mother", "Lana Del Rey is my mother");
+assertEqual(19, 22);
